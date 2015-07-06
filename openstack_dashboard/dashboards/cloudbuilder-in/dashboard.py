d6f1052d4ffce052fdff4c6984c7b640f255525a
Sriram S <sriram@innervoice.in>
2015-07-06 23:07:12 +0530

    First checkin for OpenStack Custom Dashboard Code

---
+++ openstack_dashboard/dashboards/cloudbuilder-in/dashboard.py
@@ -0,0 +1,11 @@
+from django.utils.translation import ugettext_lazy as _
+
+import horizon
+
+class CloudBuilderDashboard(horizon.Dashboard):
+    name = _("CloudBuilder")
+    slug = "cloudbuilder-in"
+    panels = ('neutron-port-list','neutron-subnet-list')
+    default_panel = 'neutron-port-list'
+
+horizon.register(CloudBuilderDashboard)
